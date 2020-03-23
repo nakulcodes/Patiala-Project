69ee2a3f16c78698075b2c9447511a2f67b46ed9
Nakul garg <nakulgarg2000@gmail.com>
2020-03-23 18:06:20 +0530

    History Wroking

--- lib/src/history.dart
+++ lib/src/history.dart
@@ -14,49 +14,82 @@ class _HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
+        child: Scaffold(
+      backgroundColor: Color(0xfffe9263),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 1,
+        height: 50,
         backgroundColor: Color(0xfffe9263),
-        bottomNavigationBar: CurvedNavigationBar(
-          index: 1,
-          height: 50,
-          backgroundColor: Color(0xfffe9263),
-          items: <Widget>[
-            Icon(Icons.home, size: 30),
-            Icon(Icons.history, size: 30),
-            Icon(Icons.account_circle, size: 30),
-          ],
-          onTap: (index) {
-            //Handle button tap
-            print(index);
-            if (index == 0) {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Dashboard()));
-            } else if (index == 2) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Account(),
-                ),
-              );
-            } else if (index == 2) {
-              print("About");
-            }
-          },
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height / 10,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30)),
+        items: <Widget>[
+          Icon(Icons.home, size: 30),
+          Icon(Icons.history, size: 30),
+          Icon(Icons.account_circle, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+          print(index);
+          if (index == 0) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Dashboard()));
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Account(),
               ),
-            ),
-          ],
-        ),
+            );
+          } else if (index == 2) {
+            print("About");
+          }
+        },
       ),
-    );
+      body: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 10-20,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)),
+            ),
+            child: Text(
+              "History!",
+              style: TextStyle(color: Colors.black, fontSize: 30),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height / 10 -10,
+            ),
+            height: MediaQuery.of(context).size.height - 100,
+            child: ListView.builder(
+                itemCount: 15,
+                itemBuilder: (BuildContext context, int index) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      child: Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Card(
+                              child: Container(
+                                child: Text("Checking\nCheck"),
+                                padding: const EdgeInsets.all(20.0),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    ));
   }
 }
